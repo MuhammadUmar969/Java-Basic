1a65b5d7ba83fe0a7ebb9bcc78f0fdda68eddc80
Muhammad Umar Aziz Khan <105535313+MuhammadUmar969@users.noreply.github.com>
2022-11-19 21:02:38 +0500

    if else

--- Prac.java
+++ Prac.java
@@ -3,27 +3,22 @@ import java.util.Scanner;
  public class Prac{
      public static void main(String [] args){
          Scanner sc = new Scanner(System.in);
-       // Calculate and print the product of three integer  
-//         int num1;
-//         int num2;
-//         int num3;
-//         System.out.println("Enter the First Number: ");
-//         num1 = sc.nextInt();
-//         System.out.println("Enter the Second Number: ");
-//         num2 = sc.nextInt();
-//         System.out.println("Enter the Third Number: ");
-//         num3 = sc.nextInt();
-//         
-//         int total = num1+num2+num3;
-//         System.out.println("The product of three integer are " +total);
-
-      //  Coverts Fahrenheit degree to Celsius
-          double celsius;
-          double tempinfahrenheit= 0.0;
-          System.out.println("Enter the Fahrenheit Value: ");
-          tempinfahrenheit= sc.nextDouble();
-          
-          celsius = (tempinfahrenheit - 32.0) * 5.0/9.0;
-          System.out.println(celsius);
+      int num1= 0 , num2 = 0, bigger;
+         System.out.println("Enter the first number");
+         num1 = sc.nextInt();
+         System.out.println("Enter the second number");
+         num2 = sc.nextInt();
+         
+         if (num1>num2){
+             bigger = num1;
+             System.out.println("The number is larger " + bigger);
+         }
+         else if (num1<num2){
+             bigger = num2;
+             System.out.println("The number is larger " + bigger);
+         }
+         else{
+             System.out.println("The numbers are equal");
+         }
      }
  }
